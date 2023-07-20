7821ee121021b84cee064c8794a918ab4df89427
Alexander Petryakov <neTp9c@yandex.ru>
2023-07-21 01:09:11 +0300

    add film and user validation

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +22,7 @@ public class FilmController {
 
     @PostMapping
     public Film create(@RequestBody final Film film) {
+        validate(film);
         if (films.containsKey(film.getId())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
@@ -30,6 +32,7 @@ public class FilmController {
 
     @PutMapping
     public Film update(@RequestBody final Film film) {
+        validate(film);
         films.put(film.getId(), film);
         return film;
     }
@@ -37,5 +40,28 @@ public class FilmController {
     @GetMapping
     public Collection<Film> findAll() {
         return films.values();
+    }
+
+    private void validate(final Film film) {
+
+        // название не может быть пустым
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // максимальная длина описания — 200 символов
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // дата релиза — не раньше 28 декабря 1895 года;
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // продолжительность фильма должна быть положительной.
+        if (film.getDuration() <= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +22,8 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody final User user) {
+        validate(user);
+        transform(user);
         if (users.containsKey(user.getId())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
@@ -30,6 +33,8 @@ public class UserController {
 
     @PutMapping
     public User update(@RequestBody final User user) {
+        validate(user);
+        transform(user);
         users.put(user.getId(), user);
         return user;
     }
@@ -37,5 +42,31 @@ public class UserController {
     @GetMapping
     public Collection<User> findAll() {
         return users.values();
+    }
+
+    private void validate(final User user) {
+
+        // электронная почта не может быть пустой и должна содержать символ @;
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //логин не может быть пустым и содержать пробелы;
+        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // дата рождения не может быть в будущем.
+        if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private void transform(final User user) {
+
+        // имя для отображения может быть пустым — в таком случае будет использован логин;
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
     }
 }
