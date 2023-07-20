5932949b0528840f1f89703f63227ee5533248b1
Alexander Petryakov <neTp9c@yandex.ru>
2023-07-21 00:43:51 +0300

    add film and user models

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+import java.time.LocalDate;
+import lombok.Data;
+
+@Data
+public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
+}
